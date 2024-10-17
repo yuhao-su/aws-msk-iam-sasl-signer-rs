a6efa88801333d634c8370cf85128bce6b513c11
Yuhao Su <31772373+yuhao-su@users.noreply.github.com>
2024-10-17 18:50:27 +0800

    chore: add explanation for consumer.recv().now_or_never() in examples (#4)
    * add explaination comments
    
    * fmt
    
    * refine
    
    * refine
    
    * fix
    
    * fix

--- examples/consumer.rs
+++ examples/consumer.rs
@@ -67,6 +67,17 @@ async fn main() {
 
     let consumer: StreamConsumer<IamConsumerContext> = config.create_with_context(context).unwrap();
 
+    // Uncomment the following code to get the partition list and assign the partitions to the consumer.
+    //
+    // Please note that it's necessary to call `consumer.recv().now_or_never()` to refresh the OAUTHBEARER token
+    // before calling `consumer.fetch_metadata()` here.
+    //
+    // > Note that before any SASL/OAUTHBEARER broker connection can succeed the application must call rd_kafka_oauthbearer_set_token()
+    // > once – either directly or, more typically, by invoking either rd_kafka_poll(), rd_kafka_consumer_poll(), rd_kafka_queue_poll(),
+    // > etc, in order to cause retrieval of an initial token to occur.
+    // See https://docs.confluent.io/platform/current/clients/librdkafka/html/rdkafka_8h.html#a988395722598f63396d7a1bedb22adaf
+    // for details.
+    //
     // assert!(consumer.recv().now_or_never().is_none());
     // let partition_list = {
     //     let mut list = TopicPartitionList::new();
